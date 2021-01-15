f785e1eff7b2502b8f4ad1e65ff2069f3af78e7f
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-15 19:06:57 +0100

    info for specific customer

--- src/components/CustomerDetailPage.jsx
+++ src/components/CustomerDetailPage.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function CustomerDetailPage(props) {
+  const customerId = props.match.params.id;
+  const [customerItem, setCustomerItem] = useState(null);
 
-    const customerId = props.match.params.id
-    return (
+  function getCustomerItem() {
+    const url = `https://frebi.willandskill.eu/api/v1/customers/${customerId}/`;
+    const token = localStorage.getItem("WEBB20");
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setCustomerItem(data));
+  }
+
+  useEffect(() => {
+    getCustomerItem();
+  }, []);
+
+  return (
+    <div>
+      <h1>Customer Detail Page</h1>
+      {customerItem ? (
         <div>
-            <h1>Customer Detail Page</h1>
+          <h1>{customerItem.name}</h1>
+          <p>{customerItem.organisationNr}</p>
+          <p>{customerItem.vatNr}</p>
+          <p>{customerItem.reference}</p>
+          <p>{customerItem.paymentTerm}</p>
+          <a href={customerItem.website} target="_blank">{customerItem.website}</a>
+          <a href={`mailto:${customerItem.email}`}>{customerItem.email}</a>
+          <p>{customerItem.phoneNumber}</p>
         </div>
-    )
+      ) : (
+        <span>Loading data..</span>
+      )}
+    </div>
+  );
 }
