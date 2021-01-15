64908889592beb13962a1a51989a9be3358c4221
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-15 18:17:35 +0100

    Route path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import CustomerDetailPage from './components/CustomerDetailPage';
+import CustomerListPage from './pages/CustomerListPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+
+
+  function getMe(){
+    const url = "https://frebi.willandskill.eu/api/v1/me/"
+    const token = localStorage.getItem("WEBB20")
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   return (
     <div>
-      
+      <Switch>
+        <Route path="/login">
+          <LoginPage/>
+        </Route>
+
+        <Route path="/customer-list">
+          <CustomerListPage/>
+        </Route>
+
+        <Route path="/customers/:id" component={CustomerDetailPage}>
+        </Route>
+      </Switch>
+
+      <hr/>
+      <button onClick={getMe}>Get Me</button>
     </div>
   );
 }
