253d15571bc408a830107af14ee6c8ea7f2f59ea
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-15 01:28:15 +0100

    Customer list page

---
+++ src/pages/CustomerListPage.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+import CustomerListItem from "../components/CustomerListItem";
+
+export default function CustomerListPage() {
+
+  const [customerList, setCustomerList] = useState([]);
+
+  useEffect (() => {
+    getCustomerList()
+  }, [])
+
+  function getCustomerList() {
+    const url = "https://frebi.willandskill.eu/api/v1/customers/";
+    const token = localStorage.getItem("WEBB20");
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setCustomerList(data.results));
+  }
+
+  return (
+    <div>
+      {customerList.map(item => {
+        return <CustomerListItem key={item.id} customerData={item}/>
+      })}
+    </div>
+  );
+}
