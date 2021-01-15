4013dea053a15d3a1616be089c948ed4dbf06750
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-15 18:15:49 +0100

    link to detail page

--- src/components/CustomerListItem.jsx
+++ src/components/CustomerListItem.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CustomerListItem({customerData}) {
-    return (
-        <div>
-            <h2>{customerData.name}</h2>
-        </div>
-    )
+export default function CustomerListItem({ customerData }) {
+  return (
+    <div>
+      <h2>
+        <Link to={`/customers/${customerData.id}`}>{customerData.name}</Link>
+      </h2>
+    </div>
+  );
 }
