93016f9d0ad2c319d4190aebefad9adac14a7788
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-16 02:52:16 +0100

    create customer function

--- src/components/CustomerCreatePage.jsx
+++ src/components/CustomerCreatePage.jsx
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function CustomerCreatePage() {
+  const [formData, setFormData] = useState({});
+  const history = useHistory()
+
+  function handleOnchange(e) {
+    console.log(e.target.name, e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   function renderInput(name, label, type) {
     return (
       <>
         <label>{label}</label>
-        <input type={type || "text"} name={name}
-
-        />
-        
+        <input type={type || "text"} name={name} onChange={handleOnchange} />
       </>
     );
+  }
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    const url = "https://frebi.willandskill.eu/api/v1/customers/"
+    const token = localStorage.getItem("WEBB20");
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then( res => res.json())
+    .then( data => {
+        history.push("/customers")
+    })
   }
 
   return (
     <div>
       <h1>Create Customer</h1>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         {renderInput("name", "Customer Name")}
         {renderInput("email", "Customer Email", "email")}
         {renderInput("organisationNr", "Organisation Number")}
@@ -27,6 +55,7 @@ export default function CustomerCreatePage() {
         {renderInput("website", "Website", "url")}
         <button type="submit">Create Customer</button>
       </form>
+      {JSON.stringify(formData)}
     </div>
   );
 }
