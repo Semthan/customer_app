1a1738a951909efec1ba27f39e75fa7621df1a28
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-16 01:31:18 +0100

    render input

--- src/components/CustomerCreatePage.jsx
+++ src/components/CustomerCreatePage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function CustomerCreatePage() {
+  function renderInput(name, label, type) {
     return (
-        <div>
-            <h1>Create</h1>
-        </div>
-    )
+      <>
+        <label>{label}</label>
+        <input type={type || "text"} name={name}
+
+        />
+        
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Create Customer</h1>
+      <form>
+        {renderInput("name", "Customer Name")}
+        {renderInput("email", "Customer Email", "email")}
+        {renderInput("organisationNr", "Organisation Number")}
+        {renderInput("paymentTerm", "Payment Term", "number")}
+        {renderInput("phoneNumber", "Phone Number", "tel")}
+        {renderInput("reference", "Reference")}
+        {renderInput("vatNr", "Vat Number")}
+        {renderInput("website", "Website", "url")}
+        <button type="submit">Create Customer</button>
+      </form>
+    </div>
+  );
 }
