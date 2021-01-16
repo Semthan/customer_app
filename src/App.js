d997713cf652f24cf183cabd2c386b315b506fca
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-16 14:33:23 +0100

    route to update page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route, Link} from 'react-router-dom'
 import CustomerCreatePage from './pages/CustomerCreatePage';
 import CustomerDetailPage from './pages/CustomerDetailPage';
 import CustomerListPage from './pages/CustomerListPage';
+import CustomerUpdatePage from './pages/CustomerUpdatePage';
 import LoginPage from './pages/LoginPage';
 
 function App() {
@@ -39,6 +40,10 @@ function App() {
 
         <Route path="/customers/create">
           <CustomerCreatePage/>
+        </Route>
+
+        <Route path="/customers/:id/edit" component={CustomerUpdatePage}> 
+
         </Route>
 
         <Route path="/customers/:id" component={CustomerDetailPage}>
