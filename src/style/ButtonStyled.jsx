442098e3c675480544471f7c1831b0b5370b97c2
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-17 22:18:28 +0100

    button style

---
+++ src/style/ButtonStyled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid gray;
+  border-radius: 5px;
+`;
+
+export { Button };
