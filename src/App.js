8d9b10284b7bac5b8a88821113aee879c4074274
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-01-17 22:20:15 +0100

    moved userData function

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
-import React, {useState} from 'react'
-import {Switch, Route, Link} from 'react-router-dom'
-import CustomerCreatePage from './pages/CustomerCreatePage';
-import CustomerDetailPage from './pages/CustomerDetailPage';
-import CustomerUpdatePage from './pages/CustomerUpdatePage';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+import React, { useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import CustomerCreatePage from "./pages/CustomerCreatePage";
+import CustomerDetailPage from "./pages/CustomerDetailPage";
+import CustomerUpdatePage from "./pages/CustomerUpdatePage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+
+import { dataContext } from "./Context/DataContext";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [customerList, setCustomerList] = useState(null);
 
 
-  function getMe(){
-    const url = "https://frebi.willandskill.eu/api/v1/me/"
-    const token = localStorage.getItem("WEBB20")
+  function fetchCustomerData() {
+    const url = "https://frebi.willandskill.eu/api/v1/customers/";
+    const token = localStorage.getItem("WEBB20");
     fetch(url, {
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
+      .then((res) => res.json())
+      .then((data) => setCustomerList(data.results));
   }
 
   return (
     <div>
+        <dataContext.Provider value={{customerList, setCustomerList, fetchCustomerData}}>
+          <Switch>
+            <Route path="/home">
+              <HomePage />
+            </Route>
 
-      <Switch>
-        <Route path="/home">
-          <HomePage/>
-        </Route>
+            <Route path="/customers/create">
+              <CustomerCreatePage />
+            </Route>
 
-        <Route path="/customers/create">
-          <CustomerCreatePage/>
-        </Route>
+            <Route
+              path="/customers/:id/edit"
+              component={CustomerUpdatePage}
+            ></Route>
 
-        <Route path="/customers/:id/edit" component={CustomerUpdatePage}> 
+            <Route path="/customers/:id" component={CustomerDetailPage}></Route>
 
-        </Route>
-
-        <Route path="/customers/:id" component={CustomerDetailPage}>
-        </Route>
-
-        <Route path="/">
-          <LoginPage/>
-        </Route>
-      </Switch>
-
-      <hr/>
-      <button onClick={getMe}>Get Me</button>
+            <Route path="/">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </dataContext.Provider>
     </div>
   );
 }
